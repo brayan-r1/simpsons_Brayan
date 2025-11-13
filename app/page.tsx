9ff0145ab0e879e88e2d3762ce1fd7e429629563
CSH-18 <hilasal@gmail.com>
2025-11-13 09:20:07 -0600

    init

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Image className="w-screen h-screen"
+        src={"/Background.jpg"}
+        width={500}  
+        height={500}  
+        alt="BackgroundSimpsons"  
+      />
+    </div>
+  );
+}
